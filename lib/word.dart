a46260a3e3f9f7551dde96c55736c42ae67b7723
Dietmar <tcodmos@gmail.com>
2022-09-18 21:32:12 +0300

    added 6 cards and connected firebase

---
+++ lib/word.dart
@@ -0,0 +1,11 @@
+class Word {
+  late final String text;
+  late final String url;
+  late bool displayText;
+
+  Word({
+    required this.text,
+    required this.url,
+    required this.displayText
+  });
+}
